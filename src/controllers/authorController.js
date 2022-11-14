5e90dec40bafe919e15405769d92191d432b6954
akashGg99 <aakkash15r@gmail.com>
2022-11-14 15:17:52 +0530

    conroller 2 added

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -13,4 +13,19 @@ const Postbloagging=async function(req,res){
       }
 }
 
+
+
+//Second Api
+
+const Handler2 = async function(req,res){
+  const filterInput =req.query
+ const result1= await blogModel.find({IsDeleted:false, isPublished:true})
+     if(result1){
+                 res.status(200).send({ msg: result1 })
+                }
+        else{
+        res.status(404).send({msg:"Nothing Found"}) 
+        }
+}
+
 module.exports.Postbloagging=Postbloagging
