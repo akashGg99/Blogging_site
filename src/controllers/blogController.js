06644152fcbc29ccd6be81b5e2dee12779353194
akashGg99 <aakkash15r@gmail.com>
2022-11-14 16:32:07 +0530

    Added getAPIs and Handlers

---
+++ src/controllers/blogController.js
@@ -0,0 +1,40 @@
+
+const authorModel = require("../Model/autherModels")
+const blogModel = require("../Model/blogModels")
+
+
+//api1..
+const Handler1 = async function (req, res) {
+    // const filterInput1 = req.query.autherId
+    // const filterInput2 = req.query.category
+  
+    const result = await blogModel.find({ IsDeleted: false, isPublished: true })
+  
+    if (result) {
+      res.status(200).send({ msg: result })
+    }
+    else {
+      res.status(404).send({ msg: "Nothing Found" })
+    }
+  }
+
+
+
+  //api 2.
+  const Handler2 = async function (req, res) {
+    const filterInput1 = req.query.autherId
+    const filterInput2 = req.query.category
+  
+    const result = await blogModel.findById({ autherId:filterInput1} || { category:filterInput2})
+  
+    if (result) {
+      res.status(200).send({ msg: result })
+    }
+    else {
+      res.status(404).send({ msg: "Nothing Found" })
+    }
+  }
+
+
+  module.exports.Handler1=Handler1
+  module.exports.Handler2=Handler2
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,16 @@
 const express=require("express")
 const router=express.Router()
 const authorController=require("../controllers/authorController")
+const blogController=require("../controllers/blogController")
+
+
 
 router.post("/bloggingpost",authorController.Postbloagging)
 
 
+
+//Get APIs
+router.get("/blogs",blogController.Handler1)
+
+router.get("/blogs",blogController.Handler2)
 
