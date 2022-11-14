f498f6cb0b144b584b53187e59b5aa94ce6ec932
Functionup-mn <nadeem2mau@gmail.com>
2022-11-14 15:45:15 +0530

    model created

---
+++ src/Models/blogModel.js
@@ -0,0 +1,39 @@
+
+  const mongoose = require('mongoose')
+  const objectId = mongoose.Schema.type.objectId
+  
+  
+  const blogSchema = new mongoose.Schema({
+        title: {
+          type: String, 
+          required: true
+        },
+        body: {
+          type: String,
+          required: true
+        },
+        authorId: {
+          type: objectId,
+          ref: "Author",
+          required: true
+        },
+        tags: [String],
+        category: {
+          type: String,
+          required: true
+        },
+        subcategory: [String-[String]],
+        isDeleted: {
+          type: Boolean,
+          default: false
+        },
+        deletedAt: String,
+        isPublished: {
+          type: String,
+          default: false
+        },
+        publishedAt: String
+  
+  },{ timestamps: true})
+  
+  module.exports = mongoose.model('Blogs', blogSchema)
