0871330596359ad46561a4a889a42340c73f2dfe
Md-Modassar <mdmodassar622@gmail.com>
2022-11-14 16:44:40 +0530

    modassar

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
+const authorMode=require('../Models/authorModel')
+const blogModel=require('../Models/blogModel')
 
 const Postbloagging=async function(req,res){
     const data=req.body
-    const authId=await autherMode.findById(data.authId)
+    const authId=await authorMode.findById(data.authId)
 
     if(authId)
       {
-        const databogging=await bobModel.create(data)
+        const databogging=await blogModel.create(data)
         res.status(201).send({data:databogging})
       }else{
         res.status(400).send({msg:"invalid authorId"})
@@ -15,17 +17,7 @@ const Postbloagging=async function(req,res){
 
 
 
-//Second Api
 
-const Handler2 = async function(req,res){
-  const filterInput =req.query
- const result1= await blogModel.find({IsDeleted:false, isPublished:true})
-     if(result1){
-                 res.status(200).send({ msg: result1 })
-                }
-        else{
-        res.status(404).send({msg:"Nothing Found"}) 
-        }
-}
+
 
 module.exports.Postbloagging=Postbloagging
